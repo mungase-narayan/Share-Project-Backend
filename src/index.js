69dc35e493264b3fa84aec2323b532767aff564e
mungase-narayan <narayanmungase03@gmail.com>
2024-07-14 22:37:57 +0530

    uptdate auth

--- src/index.js
+++ src/index.js
@@ -12,17 +12,33 @@ app.get("/", (req, res) => {
 app.post("/api/sign-up", (req, res) => {
     const { fullName, email, password, confirmPassword } = req.body;
 
-    const signUpSchema = Joi.object({
-        fullName: Joi.string().min(3).required(),
-        email: Joi.string().email().required(),
-        password: Joi.string().min(6).required(),
-        confirmPassword: Joi.ref("password"),
+    const signupSchema = Joi.object({
+         fullName: Joi.string().min(3).required(),
+         email: Joi.string().email().required(),
+         password: Joi.string().min(5).required(),
+         confirmPassword: Joi.ref("password"),
+     });
+
+    const { error } = signupSchema.validate(req.body);
+    if (error) {
+        return res.status(400).json({
+            success: false,
+            message: "Please check all fields ",
+            error: error.message,
+        });
+    }
+
+    if (password !== confirmPassword) {
+        return res.status(400).json({
+            success: false,
+            message: "Password MisMatch",
+        });
+    }
+
+    res.status(200).json({
+        message: "data stored",
     });
 
-    const { error } = signUpSchema.validate(req.body)
-    if(error){
-        return res.status(400).json({massage: "All Field Are required", error: error.details[0].message})
-    }
     return res.json({ message: "Otp Sent Successfully" });
 });
 
