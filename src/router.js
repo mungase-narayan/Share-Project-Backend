6e42408be224f6b6b8df8468ceeac073b1f915eb
mungase-narayan <narayanmungase03@gmail.com>
2024-07-14 23:35:14 +0530

    auth api update

--- src/router.js
+++ src/router.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const AuthController = require("./controllers/auth-controllers")
 const authConroller = new AuthController();
 
-router.post("/api/sign-up", authConroller.signup);
+router.post("/auth/sign-up", authConroller.signup);
 
 module.exports = router;
